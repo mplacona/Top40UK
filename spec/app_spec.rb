08a2361bcf5d44e268a0fb83570521d378c8ec9b
mplacona <marcos.placona@gmail.com>
2013-06-15 19:35:02 +0100

    Separated and added more unit tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,25 @@
 require 'spec_helper'
 
-describe 'UK Top 40 - Sinatra App' do
+describe 'UK Top 40 - Singles' do
     it "should load the UK top 40 singles" do
+        get 'top-40-single.json'
+        last_response.should be_ok
+    end
+
+    it "should retrieve a content type of json for top 40 singles" do
+        get 'top-40-single.json'
+        last_response.header['Content-Type'].should include 'application/json'
+    end
+end
+
+describe 'UK Top 40 - Albums' do
+    it "should load the UK top 40 albums" do
         get 'top-40-album.json'
         last_response.should be_ok
+    end
+
+    it "should include a conatent type o json for top 40 albums" do
+        get 'top-40-album.json'
+        last_response.header['Content-Type'].should include 'application/json'
     end
 end
