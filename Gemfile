55780cefdefc45a5a91f91d0739e24710c7a4d96
mplacona <marcos.placona@gmail.com>
2013-06-15 19:01:25 +0100

    initial project load

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# Gemfile
+source :rubygems
+  
+gem "sinatra"
+gem "nokogiri"
+
+group :test do
+    gem 'rspec', :require => 'spec'
+    gem 'rack-test'
+end
---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+
+top_40_singles  = 'http://www.bbc.co.uk/radio1/chart/singles/print'
+top_40_albuns   = 'http://www.bbc.co.uk/radio1/chart/albums/print'
+
+def parse_url (url)
+    Nokogiri::HTML(open(url)).xpath('//table[@border="1"]/tr').collect do |row|
+        position            =       row.at("td[1]/text()")
+        status              =       row.at("td[2]/text()")
+        previousPosition    =       row.at("td[3]/text()")
+        noWeeks             =       row.at("td[4]/text()")
+        artist              =       row.at("td[5]/text()")
+        title               =       row.at("td[6]/text()")
+        {:position => position, :noWeeks => noWeeks, :artist => artist, :title => title, :change => {:status => status, :previous => previousPosition}}
+    end
+end
+
+def create_output_structure(url)
+    entries = parse_url(url)
+    entries.delete_at(0) #unnecessary entry (hack)
+    time = Time.now
+    {:chartDate => time.to_i, :retrieved => Time.new(time.year, time.month, time.day).to_i, :entries => entries}
+end
+
+get '/top-40-single.json' do
+    content_type :json
+    JSON.pretty_generate(create_output_structure(top_40_singles))
+end
+
+get '/top-40-album.json' do
+    content_type :json
+    JSON.pretty_generate(create_output_structure(top_40_albuns))
+end
