d679a3410717a6de4aa0fb3b7257169493e18b2c
Leslie Hoare <iam@lesleh.co.uk>
2013-07-03 18:24:57 +0100

    Typo in settings.

--- settings.rb
+++ settings.rb
@@ -6,7 +6,7 @@ time = Time.now
 configure do
     Config = OpenStruct.new(
         :top_40_singles => 'http://www.bbc.co.uk/radio1/chart/singles/print',
-        :top_40_albuns => 'http://www.bbc.co.uk/radio1/chart/albums/print',
+        :top_40_albums => 'http://www.bbc.co.uk/radio1/chart/albums/print',
         :retrieved => Time.new(time.year, time.month, time.day).to_i,
         :getter => HTTPCacher.new( File.dirname(__FILE__) + "/cache" )
     )
