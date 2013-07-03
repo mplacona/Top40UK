f75fcfadcac319ba442917789d47747afd1e2aa7
Marcos Placona <mplacona@Marcoss-MacBook-Air.local>
2013-07-03 20:50:14 +0100

    fix typo on unit test after merging pull request

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe 'UK Top 40 - Singles' do
     it "should load the UK top 40 singles" do
-        get 'charts/gb/singles.json'
+        get 'chart/gb/singles.json'
         last_response.should be_ok
     end
 
     it "should retrieve a content type of json for top 40 singles" do
-        get 'charts/gb/singles.json'
+        get 'chart/gb/singles.json'
         last_response.header['Content-Type'].should include 'application/json'
     end
 end
 
 describe 'UK Top 40 - Albums' do
     it "should load the UK top 40 albums" do
-        get 'charts/gb/albums.json'
+        get 'chart/gb/albums.json'
         last_response.should be_ok
     end
 
     it "should include a conatent type o json for top 40 albums" do
-        get 'charts/gb/albums.json'
+        get 'chart/gb/albums.json'
         last_response.header['Content-Type'].should include 'application/json'
     end
 end
