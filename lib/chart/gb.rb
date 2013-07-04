d8251eb2598249f74555398b463899b38f1530e5
Leslie Hoare <iam@lesleh.co.uk>
2013-07-04 12:22:49 +0100

    Send the correct chart date.

--- lib/chart/gb.rb
+++ lib/chart/gb.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'time'
 
 class GBChart
 
@@ -19,30 +20,34 @@ class GBChart
 		end
 	end
 
-	def parse_url (url, key)
-	    data = Config.getter.get( url, key )
-	    Nokogiri::HTML(data).xpath('//table[@border="1"]/tr[not(th)]').collect do |row|
-	        {
-	        	:position => row.at("td[1]/text()").content.to_i,
-	        	:noWeeks  => row.at("td[4]/text()").content.to_i,
-	        	:artist   => row.at("td[5]/text()").content,
-	        	:title    => row.at("td[6]/text()").content,
-	        	:change => {
-	        		:status   => row.at("td[2]/text()").content.strip,
-	        		:previous => row.at("td[3]/text()").content.to_i
-	        	}
-	        }
+	def parse_entries(doc)
+	    doc.xpath('//table[@border="1"]/tr[not(th)]').collect do |row|
+	    {
+			:position => row.at("td[1]/text()").content.to_i,
+			:noWeeks  => row.at("td[4]/text()").content.to_i,
+			:artist   => row.at("td[5]/text()").content,
+			:title    => row.at("td[6]/text()").content,
+			:change => {
+				:status   => row.at("td[2]/text()").content.strip,
+				:previous => row.at("td[3]/text()").content.to_i
+			}
+		}
 	    end
 	end
 
 	def create_output_structure(url, key)
-	    entries = parse_url(url, key)
-	    time = Time.now
-	    {
-	    	:chartDate => time.to_i,
-	    	:retrieved => Config.retrieved,
-	    	:entries   => entries
-	    }
+		doc = Nokogiri::HTML( Config.getter.get( url, key ) )
+
+		chart_date = Time.parse doc.at('//h2/text()').content.split(' - ')[1] + " 12:00:00"
+
+		entries = parse_entries(doc)
+
+		time = Time.now
+		{
+			:chartDate => chart_date.to_i,
+			:retrieved => Config.retrieved,
+			:entries   => entries
+		}
 	end
 
 end
