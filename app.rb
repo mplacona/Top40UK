8d13eb6c95e3029d803dc8843a202ffed023e4ab
Leslie Hoare <iam@lesleh.co.uk>
2013-07-04 11:46:36 +0100

    Move the UK chart logic to lib/chart/gb.rb

--- app.rb
+++ app.rb
@@ -9,28 +9,10 @@ require File.join(File.dirname(__FILE__), 'settings')
 # Add lib directory to load path
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
+require 'chart'
+
 configure do
   set :views, "#{File.dirname(__FILE__)}/views"
-end
-
-def parse_url (url, key)
-    data = Config.getter.get( url, key )
-    Nokogiri::HTML(data).xpath('//table[@border="1"]/tr').collect do |row|
-        position            =       row.at("td[1]/text()")
-        status              =       row.at("td[2]/text()").to_s.chomp
-        previousPosition    =       row.at("td[3]/text()")
-        noWeeks             =       row.at("td[4]/text()")
-        artist              =       row.at("td[5]/text()")
-        title               =       row.at("td[6]/text()")
-        {:position => position, :noWeeks => noWeeks, :artist => artist, :title => title, :change => {:status => status, :previous => previousPosition}}
-    end
-end
-
-def create_output_structure(url, key)
-    entries = parse_url(url, key)
-    entries.delete_at(0) #unnecessary entry (hack)
-    time = Time.now
-    {:chartDate => time.to_i, :retrieved => Config.retrieved, :entries => entries}
 end
 
 get '/' do
@@ -38,15 +20,10 @@ get '/' do
 end
 
 get '/chart/:country/:type.json' do
-    content_type :json
-    case params[:type]
-    when 'singles'
-        JSON.pretty_generate(create_output_structure(Config.top_40_singles, Config.retrieved.to_s + ".single"))
-    when 'albums'
-        JSON.pretty_generate(create_output_structure(Config.top_40_albums, Config.retrieved.to_s + ".album"))
-    else
-        raise ArgumentError, 'Unknown chart type'
-    end
+    chart = Chart.create params[:country].to_sym
+    data  = chart.get params[:type].to_sym
+
+    JSON.pretty_generate data
 end
 
 # Redirect the old URLs
--- lib/chart/gb.rb
+++ lib/chart/gb.rb
@@ -3,12 +3,33 @@ class GBChart
 	def get(type)
 		case type
 		when :singles
-			raise NotImplementedError, 'Soon'
+			create_output_structure(Config.top_40_singles, Config.retrieved.to_s + ".single")
 		when :albums
-			raise NotImplementedError, 'Soon'
+			create_output_structure(Config.top_40_albums, Config.retrieved.to_s + ".album")
 		else
 			raise ArgumentError, 'Unknown chart type'
 		end
 	end
+
+	def parse_url (url, key)
+	    data = Config.getter.get( url, key )
+	    Nokogiri::HTML(data).xpath('//table[@border="1"]/tr').collect do |row|
+	        position            =       row.at("td[1]/text()")
+	        status              =       row.at("td[2]/text()").to_s.chomp
+	        previousPosition    =       row.at("td[3]/text()")
+	        noWeeks             =       row.at("td[4]/text()")
+	        artist              =       row.at("td[5]/text()")
+	        title               =       row.at("td[6]/text()")
+	        {:position => position, :noWeeks => noWeeks, :artist => artist, :title => title, :change => {:status => status, :previous => previousPosition}}
+	    end
+	end
+
+	def create_output_structure(url, key)
+	    entries = parse_url(url, key)
+	    entries.delete_at(0) #unnecessary entry (hack)
+	    time = Time.now
+	    {:chartDate => time.to_i, :retrieved => Config.retrieved, :entries => entries}
+	end
+
 
 end
