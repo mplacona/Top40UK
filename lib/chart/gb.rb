ca35670eef679a03dbb51c7c7c880f69506708b8
Leslie Hoare <iam@lesleh.co.uk>
2013-07-13 14:10:42 +0100

    Radio 1 HTML changed.

--- lib/chart/gb.rb
+++ lib/chart/gb.rb
@@ -22,17 +22,17 @@ class GBChart
 	end
 
 	def parse_entries(doc)
-	    doc.xpath('//table[@border="1"]/tr[not(th)]').collect do |row|
-	    {
-			:position => row.at("td[1]/text()").content.to_i,
-			:noWeeks  => row.at("td[4]/text()").content.to_i,
-			:artist   => row.at("td[5]/text()").content,
-			:title    => row.at("td[6]/text()").content,
-			:change => {
-				:status   => row.at("td[2]/text()").content.strip,
-				:previous => row.at("td[3]/text()").content.to_i
+	    doc.xpath('//table//tr[not(th)]').collect do |row|
+			{
+				:position => row.at("td[1]").content.to_i,
+				:noWeeks  => row.at("td[4]").content.to_i,
+				:artist   => row.at("td[5]").content,
+				:title    => row.at("td[6]").content,
+				:change => {
+					:status   => row.at("td[2]").content.strip,
+					:previous => row.at("td[3]").content.to_i
+				}
 			}
-		}
 	    end
 	end
 
@@ -43,7 +43,7 @@ class GBChart
 	def create_output_structure(url)
 		doc = Nokogiri::HTML( get_data(url) )
 
-		chart_date = Time.parse doc.at('//h2/text()').content.split(' - ')[1] + " 12:00:00"
+		chart_date = Time.parse doc.at('//h1').content.split(' - ')[1] + " 12:00:00"
 		chart_date = Time.new chart_date.year, chart_date.month, chart_date.day
 
 		entries = parse_entries(doc)
