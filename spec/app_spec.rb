72e322cfeec3b61bac32b5243b86b87618f93771
Marcos Placona <marcos.placona@gmail.com>
2013-07-28 16:47:58 +0100

    add unit tests to new US route

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -23,3 +23,15 @@ describe 'UK Top 40 - Albums' do
         last_response.header['Content-Type'].should include 'application/json'
     end
 end
+
+describe 'US Top 40 - Singles' do
+    it "should load the US top 40 singles" do
+        get 'chart/us/singles.json'
+        last_response.should be_ok
+    end
+
+    it "should retrieve a content type of json for top 40 singles" do
+        get 'chart/us/singles.json'
+        last_response.header['Content-Type'].should include 'application/json'
+    end
+end
