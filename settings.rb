54f35c3fd13f1f7b86451dd67c800f31e75e21f5
Marcos Placona <marcos.placona@gmail.com>
2013-07-30 14:50:33 +0100

    reverted caching to be enabled by default

--- settings.rb
+++ settings.rb
@@ -1,10 +1,8 @@
 require 'ostruct'
 require 'file_cache'
 
-time = Time.now
-
 configure do
     Config = OpenStruct.new(
-        :cache => FileCache.new( File.join(File.dirname(__FILE__), 'cache'), 3600, false )
+        :cache => FileCache.new( File.join(File.dirname(__FILE__), 'cache'), 3600, true )
     )
 end
